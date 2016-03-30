d75dcb38e4b29e4664a1c505d328058a9123c710
Mosh <moshfegh@live.com.au>
2016-03-30 12:05:08 +1100

    Initial commit.

---
+++ Vidly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly.Startup))]
+namespace Vidly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
