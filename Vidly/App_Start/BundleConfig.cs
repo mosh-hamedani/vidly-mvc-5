b4e6fe50998cd9d5e183175ac9d1cc904de5c040
Mosh <moshfegh@live.com.au>
2016-03-30 17:07:59 +1100

    Add a bootstrap theme.

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Vidly
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
