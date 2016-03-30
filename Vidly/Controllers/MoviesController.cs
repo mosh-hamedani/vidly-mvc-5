71b5f50be1db830b5acdfb8c011ed9c0e47ff920
Mosh <moshfegh@live.com.au>
2016-03-30 13:11:23 +1100

    MVC in Action.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+    }
+}
---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
