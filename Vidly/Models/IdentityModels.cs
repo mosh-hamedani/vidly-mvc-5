638c50c6bd42e356cef39706699429aff19be44e
Mosh <moshfegh@live.com.au>
2016-04-04 15:21:07 +1000

    Enable migrations.

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -20,6 +20,8 @@ namespace Vidly.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Customer> Customers { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
