339782cb0944bef9a12d37184810878a05bc08fa
Mosh <moshfegh@live.com.au>
2016-04-18 20:01:25 +1000

    Feed DataTable with customers API.

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -65,9 +65,7 @@ namespace Vidly.Controllers
 
         public ViewResult Index()
         {
-            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
-
-            return View(customers);
+            return View();
         }
 
         public ActionResult Details(int id)
