cdd2aa475b9993154a4bbb3ad01ab3ef449aaa0f
mohamed asem <apoasem1996@gmail.com>
2019-08-25 16:27:43 +0200

    last commit

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -1,38 +1,34 @@
-﻿using System.Web.Optimization;
+﻿using System.Web;
+using System.Web.Optimization;
 
 namespace Vidly
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/bootstrap.js",
-                        "~/scripts/bootbox.js",
-                        "~/Scripts/respond.js",
-                        "~/scripts/datatables/jquery.datatables.js",
-                        "~/scripts/datatables/datatables.bootstrap.js",
-                        "~/scripts/typeahead.bundle.js",
-                        "~/scripts/toastr.js"
-                      ));
+                        "~/Scripts/bootbox.js",
+                        "~/Scripts/datatables/jquery.dataTables.js",  // datatable
+                        "~/Scripts/datatables/dataTables.bootstrap.js" // datatable
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-lumen.css",
-                      "~/Content/bootstrap-theme.css",
-                      "~/content/datatables/css/datatables.bootstrap.css",
-                      "~/content/typeahead.css",
-                      "~/content/toastr.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css",
+                      "~/Content/Site.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap.css"  // datatable
+                      ));
         }
     }
 }
